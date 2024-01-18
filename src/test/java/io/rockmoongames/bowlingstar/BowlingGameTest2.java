b1a6eed90bf17eec28c39c7be8f720f3a72e58a9
Jessica <jessica@rockmoongames.io>
2024-01-18 16:22:54 +0000

    Add test5, test6 and test7

--- src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest2.java
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest2.java
@@ -6,4 +6,19 @@ public class BowlingGame2Test {
 
     }
 
+    @Test
+    public void Test5() {
+        
+    }
+
+    @Test
+    public void Test6() {
+        
+    }
+
+    @Test
+    public void Test7() {
+        
+    }
+
 }
